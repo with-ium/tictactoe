d52b6ecfeca9e4b662ad0977ab7ab0382b735d18
with-ium <tanway46@outlook.com>
2021-11-22 12:18:59 +0800

    Initialized variables and drew board on terminal.

--- tictactoe.java
+++ tictactoe.java
@@ -3,6 +3,27 @@ import java.util.*;
 
 public class tictactoe {
     public static void main(String args[]) {
-        
+        // Initialize variables to keep track of game
+        int player = 0, winner = 0, choice = 0, row = 0, col = 0, line = 0;
+        char[][] board = {
+                            {' ', ' ', ' '},
+                            {' ', ' ', ' '},
+                            {' ', ' ', ' '}
+                                            };
+
+        // Draw board function
+        for (int turn = 0; turn < 9 && winner == 0; turn++) {
+            System.out.println("\n");
+            System.out.println("-------------");
+            System.out.println("| " + board[0][0] + " | " + board[0][1] + " | " + board[0][2] + " | ");
+            System.out.println("-------------");
+            System.out.println("| " + board[1][0] + " | " + board[1][1] + " | " + board[1][2] + " | ");
+            System.out.println("-------------");
+            System.out.println("| " + board[2][0] + " | " + board[2][1] + " | " + board[2][2] + " | ");
+            System.out.println("-------------");
+        }
+            // Player input
+
+        // Game state function
     }
 }
